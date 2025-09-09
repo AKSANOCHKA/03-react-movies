bf97ff69e6534dda572a66ae43a82bc934598146
Oksana Skakun <ksanas180882@gmail.com>
2025-09-09 13:39:45 +0300

    add all

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,27 +1,31 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
+// API ключ из .env (должен начинаться с VITE_ в Vite проектах)
 const moviedbKey = import.meta.env.VITE_MOVIEDB_KEY;
+
 export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-
 interface FetchMovies {
-    results: Movie[]; 
+    results: Movie[];
 }
 
-export async function fetchMovies(userQuery: string):Promise<FetchMovies>{
-    const response = await axios.get<FetchMovies>('https://api.themoviedb.org/3/search/movie', {
-        params: {
-            method: 'GET',
-            query: `${userQuery}`,
-            include_adult: false,
-            page: 1,
-            language: 'en-US',
-            
-        },
-        headers: {
-            Authorization: `Bearer ${moviedbKey}`
+export async function fetchMovies(userQuery: string): Promise<FetchMovies> {
+    const response = await axios.get<FetchMovies>(
+        'https://api.themoviedb.org/3/search/movie',
+        {
+            params: {
+                query: userQuery,
+                include_adult: false,
+                page: 1,
+                language: 'en-US',
+            },
+            headers: {
+                Authorization: `Bearer ${moviedbKey}`,
+                accept: 'application/json'
+            }
         }
-    });
+    );
+
     return response.data;
 }
